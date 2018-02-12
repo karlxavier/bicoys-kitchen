36ccabb59af042888d269fc62ef4f9e10c730c5f
karlxavier <kxdc1025@yahoo.com>
2018-02-12 20:20:29 +0400

    remove some migrations

--- db/migrate/20180207211328_remove_model_plan_detail.rb
+++ db/migrate/20180207211328_remove_model_plan_detail.rb
@@ -1,5 +1,5 @@
 class RemoveModelPlanDetail < ActiveRecord::Migration[5.1]
   def change
-  	add_column :plan_fors, :plan_id, :integer
+  	# add_column :plan_fors, :plan_id, :integer
   end
 end
--- db/migrate/20180208160053_add_column_plan_id_to_plan_menus.rb
+++ db/migrate/20180208160053_add_column_plan_id_to_plan_menus.rb
@@ -1,5 +1,5 @@
 class AddColumnPlanIdToPlanMenus < ActiveRecord::Migration[5.1]
   def change
-  	add_column :plan_for_menus, :plan_id, :integer
+  	# add_column :plan_for_menus, :plan_id, :integer
   end
 end
